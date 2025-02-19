a0332580ae7d11c788278bfc28e18b3aa839f534
sara <sarasadeghnejad33@gmail.com>
2025-02-19 18:52:44 +0330

    install mongoose and connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express();
-
-app.listen('3000',()=>{
-    console.log('listening on port 3000')
-})
+const db = mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen("3000", () => {
+  console.log("listening on port 3000");
+});
