051ff36c8c3a7e5128a4ed1b94a7a63bdf3d9ab7
sara <sarasadeghnejad33@gmail.com>
2025-03-26 19:40:38 +0330

    add create listing api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "../api/routes/user.route.js";
 import authRouter from "../api/routes/auth.route.js";
+import listingRouter from "../api/routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import { STATUS_CODES } from "http";
 import { error } from "console";
@@ -23,7 +24,7 @@ app.listen("3000", () => {
 });
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-
+app.use("/api/listing", listingRouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
