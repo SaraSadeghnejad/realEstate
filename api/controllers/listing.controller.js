3b913388e231a638c7b9078667950275f0ee7d5f
sara <sarasadeghnejad33@gmail.com>
2025-03-27 11:02:24 +0330

    add update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js"
+import { errorHandler } from "../utils/error.js";
 export const createListing = async(req,res,next)=>{
     try{
      const listing = await Listing.create(req.body);
@@ -25,3 +26,14 @@ export const deleteListing = async (req, res, next) => {
       next(error);
     }
   };
+  export const updateListing = async(req,res,next)=>{
+     const listing = await Listing.findById(req.params.id);
+     if(!listing) return next(errorHandler(404,'listing not found'));
+     if(req.user.id !== listing.userRef) return next(401,'you can only delete your own listing');
+     try{
+        await Listing.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        res.status(200).json(updateListing)
+     }catch(error){
+        next(error)
+     }
+  }
